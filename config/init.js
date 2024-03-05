861b2aa8b2afb8a842fa5d3fdd1b0156d0ea7b6d
Zach <zach.Brewer.1995@gmail.com>
2024-03-04 21:31:15 -0500

    removed db_password to fix error

--- config/init.js
+++ config/init.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 (async () => {
   try {
     const { DB_NAME } = process.env;
-    const db = await mysql.createConnection('mysql://root@127.0.0.1:3306');
+    const db = await mysql.createConnection('mysql://root@localhost:3306');
     if (process.argv[2] === 'seed') {
       await db.execute(`DROP DATABASE IF EXISTS ${DB_NAME}`);
       console.log(`${DB_NAME} dropped`);
